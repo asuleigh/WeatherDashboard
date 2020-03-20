d96c2e7a1d5c25429af4e6b417a74f50c47a43ba
Amanda Surber <surberaleigh@gmail.com>
2020-03-20 18:38:38 -0400

    added js variables and urls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
-var OW_API_KEY = "166a433c57516f51dfab1f7edaed8413";
-var weather
+var cityInput= $("#searchInput").val();
+
+var OW_API_KEY= "166a433c57516f51dfab1f7edaed8413";
+
+let date= new Date();
+
+queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + OW_API_KEY;
+
+var url= "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + OW_API_KEY;
