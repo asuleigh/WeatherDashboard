589f6c1b6f8ea2eecd98567053339bb4b19dfd02
Amanda Surber <surberaleigh@gmail.com>
2020-03-20 22:04:19 -0400

    daily forecast is rendering when searching for a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $('#citySubmit').on('click', function(e) {
     event.preventDefault(e);
     // Grabs and creates variable for city search input
-    var cityInput= $('#searchInput').val();
+    var cityInput= $('#cityInput').val();
     // API Key
     var OW_API_KEY= "761655843181856ce1e226b5e0cd7d37";
     // URLs for calls
@@ -11,12 +11,13 @@ $('#citySubmit').on('click', function(e) {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    })
+    .then(function(response) {
 
         console.log(response);
 
-        var nextCard= $('<div>').addClass('card mt-4 col-sm-9'),
-            nextBody= $('<div>').addClass('card-body p-3'),
+        var nextCard= $('<div>').addClass('card col-sm-9 mt-3'),
+            nextBody= $('<div>').addClass('card-body py-1'),
 
             // Append the body to the card
             appendedCard= $(nextCard).append(nextBody),
@@ -35,15 +36,17 @@ $('#citySubmit').on('click', function(e) {
             url: uviURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + 
             OW_API_KEY + "&lat=" +lat+ "&lon=" +lon,
             method: "GET"
-            }).then(function(data){
+            })
+            .then(function(data){
             addUVI = $('<p>').attr('class','card-text')
             .append("UV Index: " + data.value);
+
             $(appendedCard).append(addUVI);
             });
 
         //
         title = $('<h2>').attr('class','card-title').append( response.name + " (" + currentTime + ") "  ).append(weatherIcon);
-        temp = $('<p>').attr('class', 'card-text').append("Current Temp: " + response.main.temp + "°F");
+        temp = $('<p>').attr('class', 'card-text').append("Temperature: " + response.main.temp + "°F");
         humidity = $('<p>').attr('class', 'card-text').append("Humidity: " + response.main.humidity + "%");
         windSpeed = $('<p>').attr('class', 'card-text').append("Wind Speed: " + response.wind.speed + "mph");
 
@@ -51,12 +54,6 @@ $('#citySubmit').on('click', function(e) {
         $(appendedCard).append(title, temp, humidity, windSpeed);
     
         //
-        $('dayForecast').append(appendedCard);
+        $('#dayForecast').append(appendedCard);
     });
 });
-
-// 
-// $('#citySubmit').on('click', function(event){
-//     event.preventDefault();
-//     weatherSearch();
-// });
