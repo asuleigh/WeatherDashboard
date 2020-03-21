5cd3d5cf53b21286904b86a3695419c6087c5351
Amanda Surber <surberaleigh@gmail.com>
2020-03-20 22:27:59 -0400

    cleaned up code. Updated comments in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// onClick function for rendering forecast of a single day
 $('#citySubmit').on('click', function(e) {
     event.preventDefault(e);
     // Grabs and creates variable for city search input
@@ -8,30 +9,35 @@ $('#citySubmit').on('click', function(e) {
     queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&units=imperial&appid=" + OW_API_KEY;
     // forecastURL= "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + OW_API_KEY;
     
+    // Ajax call to retrieve information from the OpenWeatherMap API to 
+    // use in creating a card to display the current weather
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function(response) {
+        // check work
+        // console.log(response);
 
-        console.log(response);
-
+        // Creates and sets a variable for each new card and card body
         var nextCard= $('<div>').addClass('card col-sm-9 mt-3 text-dark'),
             nextBody= $('<div>').addClass('card-body py-1'),
 
-            // Append the body to the card
+            // Appends the body to the card
             appendedCard= $(nextCard).append(nextBody),
 
-            // Get current time
+            // Gets current time
             currentTime= moment().format('MM/DD/YY'),
 
+            // Grabs source for the weather icons
             weatherIcon= $("<img src='http://openweathermap.org/img/wn/" + 
             response.weather[0].icon + ".png' > "),
             
+            // Sets lat and lon
             lat = response.coord.lat,
             lon = response.coord.lon;
 
-        // getting and appending UV Index using coords from initial ajax call
+        // Ajax call for getting the UV Index by using coords for each city
         $.ajax({
             url: uviURL = "http://api.openweathermap.org/data/2.5/uvi?appid=" + 
             OW_API_KEY + "&lat=" +lat+ "&lon=" +lon,
@@ -44,16 +50,19 @@ $('#citySubmit').on('click', function(e) {
             $(appendedCard).append(addUVI);
             });
 
-        //
+        // Creates elements and appends attributes for the forecast card
         title = $('<h2>').attr('class','card-title').append( response.name + " (" + currentTime + ") "  ).append(weatherIcon);
         temp = $('<p>').attr('class', 'card-text').append("Temperature: " + response.main.temp + "°F");
         humidity = $('<p>').attr('class', 'card-text').append("Humidity: " + response.main.humidity + "%");
         windSpeed = $('<p>').attr('class', 'card-text').append("Wind Speed: " + response.wind.speed + "mph");
 
-        // 
+        // Appends the attributes set above to the card
         $(appendedCard).append(title, temp, humidity, windSpeed);
     
-        //
+        // Appends each new forecast call as a new card
         $('#dayForecast').append(appendedCard);
     });
+
+    // Clears the city input after the search function is carried out
+    $("#cityInput").val("");
 });
